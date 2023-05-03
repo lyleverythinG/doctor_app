a0d0d002cda77440658cf98d697ec81692c579ce
lyleverythinG <75658617+lyleverythinG@users.noreply.github.com>
2023-05-04 07:04:05 +0800

    finished setup dio for http request (#3)
    - also renamed home folder into user_home

---
+++ lib/features/user_home/data/repository/user_repo.dart
@@ -0,0 +1,53 @@
+import 'package:dio/dio.dart';
+import 'package:doctor_app/core/network/api/user_api.dart';
+import 'package:doctor_app/core/network/dio/dio_exception.dart';
+import 'package:doctor_app/features/user_home/domain/model/user_model.dart';
+
+class UserRepo {
+  final API userApi;
+
+  UserRepo({required this.userApi});
+
+  Future<UserModel> addDoctor(
+      String doctorName, String type, String yearsOfExp) async {
+    try {
+      final response = await userApi.addDoctor(doctorName, type, yearsOfExp);
+      return UserModel.fromJson(response.data);
+    } on DioError catch (e) {
+      final errorMessage = DioExceptions.dioError(e).toString();
+      throw errorMessage;
+    }
+  }
+
+  Future<UserModel> getUser(String id) async {
+    try {
+      final response = await userApi.getUser(id);
+      final user = UserModel.fromJson(response.data);
+      return user;
+    } on DioError catch (e) {
+      final errorMessage = DioExceptions.dioError(e).toString();
+      throw errorMessage;
+    }
+  }
+
+  Future<UserModel> updateDoctorInfo(
+      int id, String doctorName, String type, String yearsOfExp) async {
+    try {
+      final response =
+          await userApi.updateDoctorInfo(id, doctorName, type, yearsOfExp);
+      return UserModel.fromJson(response.data);
+    } on DioError catch (e) {
+      final errorMessage = DioExceptions.dioError(e).toString();
+      throw errorMessage;
+    }
+  }
+
+  Future<void> deleteUser(int id) async {
+    try {
+      await userApi.deleteUser(id);
+    } on DioError catch (e) {
+      final errorMessage = DioExceptions.dioError(e).toString();
+      throw errorMessage;
+    }
+  }
+}
