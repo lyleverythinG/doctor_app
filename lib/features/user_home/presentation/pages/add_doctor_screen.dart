6533fe6b520dee3547196f5ed5115eaeeb2ffdb8
lyleverythinG <lylecarcedo@gmail.com>
2023-05-04 07:31:06 +0800

    added add doctor tab.
    - created add doctor screen also.

---
+++ lib/features/user_home/presentation/pages/add_doctor_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AddDoctorScreen extends StatelessWidget {
+  const AddDoctorScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Text('add doctor screen'),
+    );
+  }
+}
