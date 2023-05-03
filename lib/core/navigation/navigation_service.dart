8c06cd3e9e0c352479c06558ed4b208c677ae805
lyleverythinG <lylecarcedo@gmail.com>
2023-05-03 18:13:39 +0800

    finished initial project setup
     - added navigation
    - added splash screen
    - added service locator
    - created home screen.

---
+++ lib/core/navigation/navigation_service.dart
@@ -0,0 +1,57 @@
+import 'package:doctor_app/core/navigation/navigation_animation.dart';
+import 'package:doctor_app/features/splash_screen/splash_screen.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class NavigationService {
+  final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+  Future<dynamic> pushNavigateTo(String routeName) {
+    return navigatorKey.currentState!.pushNamed(routeName);
+  }
+
+  Future<dynamic> pushReplaceNavigateTo(String routeName, {Object? args}) {
+    return navigatorKey.currentState!
+        .pushReplacementNamed(routeName, arguments: args);
+  }
+
+  Future<dynamic> pushNavigateToWidget(Route<dynamic> route) {
+    return navigatorKey.currentState!.push(route);
+  }
+
+  Future<dynamic> pushReplaceNavigateToWidget(Route<dynamic> route) {
+    return navigatorKey.currentState!.pushReplacement(route);
+  }
+
+  bool get isFirst => !navigatorKey.currentState!.canPop();
+
+  goBack() {
+    return navigatorKey.currentState!.pop();
+  }
+
+  goBackToFirst() {
+    return navigatorKey.currentState!.popUntil((route) => route.isFirst);
+  }
+
+  static PageRoute getPageRoute(Widget child, RouteSettings settings) {
+    return FadeRoute(page: child, routeName: settings.name);
+  }
+
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    if (settings.arguments != null) {
+      if (kDebugMode) {
+        print(settings.arguments);
+      }
+    }
+    if (kDebugMode) {
+      print(settings.name);
+    }
+
+    switch (settings.name) {
+      case SplashScreen.route:
+        return getPageRoute(const SplashScreen(), settings);
+      default:
+        return getPageRoute(const SizedBox.shrink(), settings);
+    }
+  }
+}
---
+++ lib/features/splash_screen/splash_screen.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+import 'package:doctor_app/core/locator/service_locator.dart';
+import 'package:doctor_app/core/navigation/navigation_animation.dart';
+import 'package:doctor_app/features/home/presentation/pages/home.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  static const String route = 'splash';
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  late final Timer _timer;
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer(const Duration(seconds: 3), () async {
+      await kNavigator
+          .pushReplaceNavigateToWidget(SlideLeftRoute(page: const HomePage()));
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          child: Lottie.asset(
+            'assets/images/splash_screen.json',
+            repeat: false,
+          ),
+        ),
+      ),
+    );
+  }
+}
