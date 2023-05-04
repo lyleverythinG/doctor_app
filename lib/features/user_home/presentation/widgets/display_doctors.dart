96eb84422898c9ac1c5fd4bd00739282f80fc245
lyleverythinG <lylecarcedo@gmail.com>
2023-05-05 06:40:27 +0800

    delete user implemented

--- lib/features/user_home/presentation/widgets/display_doctors.dart
+++ lib/features/user_home/presentation/widgets/display_doctors.dart
@@ -1,3 +1,4 @@
+import 'package:doctor_app/core/constants/constants.dart';
 import 'package:doctor_app/core/locator/service_locator.dart';
 import 'package:doctor_app/core/navigation/navigation_animation.dart';
 import 'package:doctor_app/features/user_home/domain/model/user_model.dart';
@@ -27,28 +28,43 @@ class DisplayDoctors extends StatelessWidget {
                 physics: const BouncingScrollPhysics(),
                 itemCount: state.users.length,
                 itemBuilder: ((context, index) {
-                  return GestureDetector(
-                    onTap: () {
-                      kNavigator.pushNavigateToWidget(SlideLeftRoute(
-                          page: EditDoctorInfoScreen(
-                        userIndex: index,
-                        userModel: UserModel(
-                            createdAt: state.users[index].createdAt,
-                            doctorName: state.users[index].doctorName,
-                            doctorType: state.users[index].doctorType,
-                            id: state.users[index].id,
-                            isFavorite: state.users[index].isFavorite,
-                            updatedAt: state.users[index].updatedAt,
-                            yearsOfExp: state.users[index].yearsOfExp),
-                      )));
+                  // swipe left or right to delete a user from the list.
+                  return Dismissible(
+                    key: Key(state.users[index].id!),
+                    background: Container(
+                      color: Constants.kRedAccent,
+                      child: const Center(
+                        child: Icon(Icons.delete),
+                      ),
+                    ),
+                    //TODO: check if index or user id should be passed
+                    onDismissed: (direction) async {
+                      context.read<UserBloc>().add(DeleteDoctor(
+                          index: index, userId: state.users[index].id!));
                     },
-                    onDoubleTap: () {},
-                    child: DoctorCard(
-                        profileImg: 'assets/images/doctor1.jpg',
-                        name: state.users[index].doctorName!,
-                        doctorType: state.users[index].doctorType!,
-                        addedSince: state.users[index].createdAt!,
-                        yearsOfExp: state.users[index].yearsOfExp!),
+                    child: GestureDetector(
+                      onTap: () {
+                        kNavigator.pushNavigateToWidget(SlideLeftRoute(
+                            page: EditDoctorInfoScreen(
+                          userIndex: index,
+                          userModel: UserModel(
+                              createdAt: state.users[index].createdAt,
+                              doctorName: state.users[index].doctorName,
+                              doctorType: state.users[index].doctorType,
+                              id: state.users[index].id,
+                              isFavorite: state.users[index].isFavorite,
+                              updatedAt: state.users[index].updatedAt,
+                              yearsOfExp: state.users[index].yearsOfExp),
+                        )));
+                      },
+                      onDoubleTap: () {},
+                      child: DoctorCard(
+                          profileImg: 'assets/images/doctor1.jpg',
+                          name: state.users[index].doctorName!,
+                          doctorType: state.users[index].doctorType!,
+                          addedSince: state.users[index].createdAt!,
+                          yearsOfExp: state.users[index].yearsOfExp!),
+                    ),
                   );
                 }),
               ),
