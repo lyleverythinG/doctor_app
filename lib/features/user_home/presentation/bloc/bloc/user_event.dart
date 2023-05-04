57a861180a5687f68c4149c94df016162dd14e21
lyleverythinG <75658617+lyleverythinG@users.noreply.github.com>
2023-05-05 05:15:50 +0800

    Added Update doctor information UI (#7)
    * added loading state.
    
    - For loading purposes.
    
    * added update doctor information UI
    
    - Also added functionality to save time.

--- lib/features/user_home/presentation/bloc/bloc/user_event.dart
+++ lib/features/user_home/presentation/bloc/bloc/user_event.dart
@@ -8,11 +8,53 @@ class CreateUserDoctor extends UserEvent {
   final String doctorName;
   final String doctorType;
   final String yearsOfExperience;
-
-  const CreateUserDoctor(
-      {required this.doctorName,
-      required this.doctorType,
-      required this.yearsOfExperience});
+  const CreateUserDoctor({
+    required this.doctorName,
+    required this.doctorType,
+    required this.yearsOfExperience,
+  });
   @override
-  List<Object> get props => [doctorName, doctorType, yearsOfExperience];
+  List<Object> get props => [
+        doctorName,
+        doctorType,
+        yearsOfExperience,
+      ];
+}
+
+class GetDoctorInfo extends UserEvent {
+  final String userId;
+  const GetDoctorInfo({
+    required this.userId,
+  });
+  @override
+  List<Object> get props => [
+        userId,
+      ];
+}
+
+class UpdateDoctorInfo extends UserEvent {
+  final String userId;
+  final int userIndex;
+  final String doctorName;
+  final String doctorType;
+  final String yearsOfExp;
+  final String createdAt;
+
+  const UpdateDoctorInfo(
+      {required this.userId,
+      required this.userIndex,
+      required this.doctorName,
+      required this.doctorType,
+      required this.yearsOfExp,
+      required this.createdAt});
+  @override
+  List<Object> get props =>
+      [userId, userIndex, doctorName, doctorType, yearsOfExp, createdAt];
+}
+
+class UserUpdate extends UserEvent {
+  final List<UserModel> users;
+  const UserUpdate({required this.users});
+  @override
+  List<Object> get props => [users];
 }
