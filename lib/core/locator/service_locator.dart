1c8d4af1f5175730f8437912998632a6b7d140c6
lyleverythinG <lylecarcedo@gmail.com>
2023-05-04 12:01:54 +0800

    updated service_locator.dart
    - setting up Dio, and user repo.

--- lib/core/locator/service_locator.dart
+++ lib/core/locator/service_locator.dart
@@ -1,4 +1,8 @@
+import 'package:dio/dio.dart';
 import 'package:doctor_app/core/navigation/navigation_service.dart';
+import 'package:doctor_app/core/network/api/user_api.dart';
+import 'package:doctor_app/core/network/dio/dio_client.dart';
+import 'package:doctor_app/features/user_home/data/repository/user_repo.dart';
 import 'package:get_it/get_it.dart';
 
 GetIt locator = GetIt.instance;
@@ -9,4 +13,8 @@ final kNavigator = locator<NavigationService>();
 void setUpServiceLocator() {
   // Services
   locator.registerSingleton<NavigationService>(NavigationService());
+  locator.registerSingleton(Dio());
+  locator.registerSingleton(DioClient(dio: locator<Dio>()));
+  locator.registerSingleton(API(dioClient: locator<DioClient>()));
+  locator.registerSingleton(UserRepo(userApi: locator.get<API>()));
 }
