f48e60288e82f6d2a877cc6781d74abd85a654ee
lyleverythinG <lylecarcedo@gmail.com>
2023-05-05 13:32:19 +0800

    created custom search delegate screen.

---
+++ lib/features/sorted_doctors/presentation/pages/appbar_searched_screen.dart
@@ -0,0 +1,58 @@
+import 'package:doctor_app/features/sorted_doctors/presentation/widgets/search_delegate_list_view.dart';
+import 'package:doctor_app/features/user_home/domain/model/user_model.dart';
+import 'package:flutter/material.dart';
+
+class CustomSearchDelegate extends SearchDelegate {
+  List<UserModel> listOfDoctors;
+  CustomSearchDelegate({required this.listOfDoctors});
+  //this is the list you want to search
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        onPressed: () => 'query',
+        icon: const Icon(Icons.clear),
+      ),
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+        onPressed: () {
+          //closes the search page and opens back the original page.
+          //this is the part that we can leave and close the search bar.
+          close(context, null);
+        },
+        icon: const Icon(Icons.arrow_back));
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    //this is where we create the UI for the results;
+    List<UserModel> matchedKeyword = [];
+    for (var doctor in listOfDoctors) {
+      // searched bar checks for matching doctor type or name.
+      if (doctor.doctorType!.toLowerCase().contains(query.toLowerCase()) ||
+          doctor.doctorName!.toLowerCase().contains(query.toLowerCase())) {
+        matchedKeyword.add(doctor);
+      }
+    }
+    return SearchDelegateListView(
+        listOfDoctors: listOfDoctors, matchedKeyword: matchedKeyword);
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<UserModel> matchedKeyword = [];
+    for (var doctor in listOfDoctors) {
+      // searched bar checks for matching doctor type or name.
+      if (doctor.doctorType!.toLowerCase().contains(query.toLowerCase()) ||
+          doctor.doctorName!.toLowerCase().contains(query.toLowerCase())) {
+        matchedKeyword.add(doctor);
+      }
+    }
+    return SearchDelegateListView(
+        listOfDoctors: listOfDoctors, matchedKeyword: matchedKeyword);
+  }
+}
