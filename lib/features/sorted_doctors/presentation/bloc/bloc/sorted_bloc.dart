4df80c2e43aa3c6b67891cbe4fd4d6feccbf42a2
lyleverythinG <lylecarcedo@gmail.com>
2023-05-05 22:27:12 +0800

    Added try catch for error handling
    - Added error handling on blocs.

--- lib/features/sorted_doctors/presentation/bloc/bloc/sorted_bloc.dart
+++ lib/features/sorted_doctors/presentation/bloc/bloc/sorted_bloc.dart
@@ -2,6 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:doctor_app/features/user_home/data/repository/sort_functions.dart';
 import 'package:doctor_app/features/user_home/domain/model/user_model.dart';
 import 'package:equatable/equatable.dart';
+import 'dart:developer' as developer;
 
 part 'sorted_event.dart';
 part 'sorted_state.dart';
@@ -10,13 +11,18 @@ class SortedBloc extends Bloc<SortedEvent, SortedState> {
   final List<UserModel> sortedResult = [];
   SortedBloc() : super(SortedInitial()) {
     on<SortByDoctorType>((event, emit) {
-      emit(const SortedUsersLoadingState());
-      final sortedDoctors = SortFunctions.sortDoctorsByType(
-          doctorType: event.doctorType, doctors: event.doctors);
-      if (sortedDoctors.isNotEmpty) {
-        emit(SortedDoctorsResult(sortedDoctors: sortedDoctors));
-      } else {
-        emit(const EmptySortedResult());
+      try {
+        emit(const SortedUsersLoadingState());
+        final sortedDoctors = SortFunctions.sortDoctorsByType(
+            doctorType: event.doctorType, doctors: event.doctors);
+        if (sortedDoctors.isNotEmpty) {
+          emit(SortedDoctorsResult(sortedDoctors: sortedDoctors));
+        } else {
+          emit(const EmptySortedResult());
+        }
+      } catch (e) {
+        developer.log('Error Sorting Users: $e',
+            name: 'ErrorSortingFunctionality');
       }
     });
   }
