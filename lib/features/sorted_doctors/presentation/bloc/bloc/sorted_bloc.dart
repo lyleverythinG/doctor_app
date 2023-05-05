6e18b93f97c464316fe0b97dbf581bfa8d087000
lyleverythinG <lylecarcedo@gmail.com>
2023-05-05 08:54:58 +0800

    created bloc and added sort functionality
    - Setup also bloc on main.dart

---
+++ lib/features/sorted_doctors/presentation/bloc/bloc/sorted_bloc.dart
@@ -0,0 +1,23 @@
+import 'package:bloc/bloc.dart';
+import 'package:doctor_app/features/user_home/data/repository/sort_functions.dart';
+import 'package:doctor_app/features/user_home/domain/model/user_model.dart';
+import 'package:equatable/equatable.dart';
+
+part 'sorted_event.dart';
+part 'sorted_state.dart';
+
+class SortedBloc extends Bloc<SortedEvent, SortedState> {
+  final List<UserModel> sortedResult = [];
+  SortedBloc() : super(SortedInitial()) {
+    on<SortByDoctorType>((event, emit) {
+      emit(const LoadingState());
+      final sortedDoctors = SortFunctions.sortDoctorsByType(
+          doctorType: event.doctorType, doctors: event.doctors);
+      if (sortedDoctors.isNotEmpty) {
+        emit(SortedDoctorsResult(sortedDoctors: sortedDoctors));
+      } else {
+        emit(const EmptySortedResult());
+      }
+    });
+  }
+}
