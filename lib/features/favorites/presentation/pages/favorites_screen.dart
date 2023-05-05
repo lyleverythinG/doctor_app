7a93a6f9481cf372386ad7c2d393370f3aa792c0
lyleverythinG <lylecarcedo@gmail.com>
2023-05-05 20:52:59 +0800

    Added Comment for readability.

--- lib/features/favorites/presentation/pages/favorites_screen.dart
+++ lib/features/favorites/presentation/pages/favorites_screen.dart
@@ -23,6 +23,7 @@ class FavoritesScreen extends StatelessWidget {
                       .where((user) => user.isFavorite == 'true')
                       .isNotEmpty;
                   if (containsFavorites) {
+                    // displays listview cards if it contains favorites
                     return Expanded(
                       child: ListView.builder(
                         itemCount: state.users.length,
@@ -52,6 +53,7 @@ class FavoritesScreen extends StatelessWidget {
                   }
                 }
                 if (state is LoadingState) {
+                  // display loading UI when fetching users.
                   return SizedBox(
                       height: MediaQuery.of(context).size.height * 0.75,
                       child: const Loading());
