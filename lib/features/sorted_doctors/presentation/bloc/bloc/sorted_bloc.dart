48da9af85a6325c13460654e10d699dbc4f27c03
lyleverythinG <lylecarcedo@gmail.com>
2023-05-06 09:10:50 +0800

    removed unnecessary spacing

--- lib/features/sorted_doctors/presentation/bloc/bloc/sorted_bloc.dart
+++ lib/features/sorted_doctors/presentation/bloc/bloc/sorted_bloc.dart
@@ -3,7 +3,6 @@ import 'package:doctor_app/features/user_home/data/repository/sort_functions.dar
 import 'package:doctor_app/features/user_home/domain/model/user_model.dart';
 import 'package:equatable/equatable.dart';
 import 'dart:developer' as developer;
-
 part 'sorted_event.dart';
 part 'sorted_state.dart';
 
