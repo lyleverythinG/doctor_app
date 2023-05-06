b531f181da51b975f2dbc59fe4718ca6f6c9ba7c
lyleverythinG <lylecarcedo@gmail.com>
2023-05-06 13:54:36 +0800

    Refactored code for unit testing.
    - Added a different class for doctors dummy data

--- test/unit_testing/sort_function_test.dart
+++ test/unit_testing/sort_function_test.dart
@@ -2,46 +2,13 @@ import 'package:doctor_app/features/user_home/data/repository/sort_functions.dar
 import 'package:doctor_app/features/user_home/domain/model/user_model.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'doctors_dummy_data.dart';
+
 void main() {
-  // Added dummy data for testing sort/filter functionality.
-  final List<UserModel> doctors = [
-    UserModel(
-        doctorName: 'test',
-        doctorType: 'Cardiologist',
-        id: '1',
-        isFavorite: 'false',
-        createdAt: 'testDate',
-        updatedAt: 'testDate',
-        yearsOfExp: '5'),
-    UserModel(
-        doctorName: 'test',
-        doctorType: 'Cardiologist',
-        id: '1',
-        isFavorite: 'false',
-        createdAt: 'testDate',
-        updatedAt: 'testDate',
-        yearsOfExp: '5'),
-    UserModel(
-        doctorName: 'test',
-        doctorType: 'Neurologist',
-        id: '1',
-        isFavorite: 'false',
-        createdAt: 'testDate',
-        updatedAt: 'testDate',
-        yearsOfExp: '5'),
-    UserModel(
-        doctorName: 'test',
-        doctorType: 'Pediatrician',
-        id: '1',
-        isFavorite: 'false',
-        createdAt: 'testDate',
-        updatedAt: 'testDate',
-        yearsOfExp: '5'),
-  ];
   test('Test Sort Doctors by Type Functionality', () {
     // In this test, we passed 'Cardiologist' in the sortDoctorsByType function.
     final List<UserModel> doctorsResult = SortFunctions.sortDoctorsByType(
-        doctors: doctors, doctorType: 'Cardiologist');
+        doctors: DoctorDummyData.doctors, doctorType: 'Cardiologist');
     // this code will return false if any of the doctor type is not Cardiologist from the result returned by the function.
     bool allDoctorTypeIsCardio =
         !doctorsResult.any((doctor) => doctor.doctorType != "Cardiologist");
