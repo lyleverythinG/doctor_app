254fac669109d91d9ef813de9c0b57b57bce8ea2
lyleverythinG <lylecarcedo@gmail.com>
2023-05-06 18:00:22 +0800

    Sorted packages related to user_home feature

---
+++ lib/core/packages/user_home_feature_packages/user_home_related_packages.dart
@@ -0,0 +1,6 @@
+export 'package:doctor_app/features/user_home/data/repository/user_repo.dart';
+export 'package:doctor_app/features/user_home/presentation/bloc/bloc/user_bloc.dart';
+export 'package:doctor_app/features/user_home/presentation/pages/home.dart';
+export 'package:doctor_app/features/user_home/domain/model/user_model.dart';
+export 'package:doctor_app/features/user_home/presentation/widgets/doctor_card.dart';
+export 'package:doctor_app/features/user_home/data/repository/sort_functions.dart';
