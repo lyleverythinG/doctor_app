78feda4fc965d8919ad750dfece27e1bcab94b29
lyleverythinG <lylecarcedo@gmail.com>
2023-05-06 16:58:04 +0800

    Extends HiveObject to UserApiModel
    - Added adapter as well.
    - Local Storage Functionality Implemented.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:doctor_app/core/constants/constants.dart';
+import 'package:doctor_app/core/local_storage/adaptor/user_adaptor.dart';
 import 'package:doctor_app/core/locator/service_locator.dart';
 import 'package:doctor_app/core/navigation/navigation_service.dart';
 import 'package:doctor_app/core/network/api/user_api.dart';
@@ -6,13 +7,18 @@ import 'package:doctor_app/features/sorted_doctors/presentation/bloc/bloc/sorted
 import 'package:doctor_app/features/splash_screen/splash_screen.dart';
 import 'package:doctor_app/features/user_home/data/repository/user_repo.dart';
 import 'package:doctor_app/features/user_home/presentation/bloc/bloc/user_bloc.dart';
+import 'package:doctor_app/features/users_from_api/domain/users_api_model.dart';
 import 'package:doctor_app/features/users_from_api/presentation/bloc/bloc/user_api_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(UserApiModelAdapter());
+  await Hive.openBox<UserApiModel>('reqresUsers');
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
