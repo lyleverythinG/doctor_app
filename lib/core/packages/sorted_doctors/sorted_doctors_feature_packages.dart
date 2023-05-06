346ee02823d7745710994e48a3ecd59cb23953a4
lyleverythinG <lylecarcedo@gmail.com>
2023-05-06 18:13:08 +0800

    Sorted Packages related to sorted_doctors

---
+++ lib/core/packages/sorted_doctors/sorted_doctors_feature_packages.dart
@@ -0,0 +1,4 @@
+export 'package:doctor_app/features/sorted_doctors/presentation/bloc/bloc/sorted_bloc.dart';
+export 'package:doctor_app/features/sorted_doctors/presentation/pages/appbar_searched_screen.dart';
+export 'package:doctor_app/features/sorted_doctors/presentation/pages/searched_doctor_screen.dart';
+export 'package:doctor_app/features/sorted_doctors/presentation/pages/filtered_doctor_type_screen.dart';
